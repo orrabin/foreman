ccdd535bedacd1365b91f9224bbc7ad7e838f480
Tom Caspy <tcaspy@gmail.com>
2015-03-03 11:51:01 +0200

    fix mailer tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -323,6 +323,7 @@ end
 Spork.each_run do
   # This code will be run each time you run your specs.
   class ActionController::TestCase
+    self.use_transactional_fixtures = true
     setup :setup_set_script_name, :set_api_user, :reset_setting_cache
     teardown :reset_setting_cache, :remove_raise_strong_parameters
 
@@ -353,6 +354,7 @@ Spork.each_run do
   end
 
   class ActionDispatch::IntegrationTest
+    self.use_transactional_fixtures = true
     setup :login_admin
 
     teardown do
